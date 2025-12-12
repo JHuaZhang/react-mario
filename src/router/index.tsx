56bc1d5700e51ac13c14204ee284179421b1b46a
JHuaZhang <hn131417@163.com>
2025-12-12 18:42:19 +0800

    feat: 无用内容删除

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Designer from '@/pages/Designer/index';
 import Builder from '@/pages/Builder/index';
 
 const AppRouter: React.FC = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/designer" replace />} />
         <Route path="/designer" element={<Designer />} />
         <Route path="/builder" element={<Builder />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
