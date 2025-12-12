c1da027f2378e10c5d1bdb0655b9ebd22c2233f0
JHuaZhang <hn131417@163.com>
2025-12-12 18:37:15 +0800

    feat: test use github action

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,22 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ command }) => {
+  const base = command === 'serve' ? '/' : '/react-mario/';
+
+  return {
+    plugins: [react()],
+    base: base,
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    port: 2345,
-    host: '0.0.0.0',
-    strictPort: false,
-  },
+    server: {
+      port: 2345,
+      host: '0.0.0.0',
+      strictPort: false,
+      historyApiFallback: true,
+    },
+  };
 });
