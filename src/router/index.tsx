b21233fd270873716855d02266cb17efd488b46b
JHuaZhang <hn131417@163.com>
2025-12-12 18:28:32 +0800

    test: use HashRouter instead of BrowserRouter

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Designer from '@/pages/Designer/index';
 import Builder from '@/pages/Builder/index';
 
 const AppRouter: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/designer" replace />} />
         <Route path="/designer" element={<Designer />} />
         <Route path="/builder" element={<Builder />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
