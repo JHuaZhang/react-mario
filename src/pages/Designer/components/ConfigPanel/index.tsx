92da3ac97136a7140a31a28bb950d3ed4b660e61
JHuaZhang <hn131417@163.com>
2025-12-27 15:07:26 +0800

    feat: 删除使用zustand，改用mobx

--- src/pages/Designer/components/ConfigPanel/index.tsx
+++ src/pages/Designer/components/ConfigPanel/index.tsx
@@ -1,15 +1,24 @@
 import { useMemo } from 'react';
 import { Drawer } from 'antd';
+import { observer } from 'mobx-react-lite';
 import { useStore } from '@/store/useStore';
+import { getComponentCategory } from '../ComponentPanel/config/componentCategories';
 
-const ConfigPanel = () => {
+const ConfigPanel = observer(() => {
   const { setOpenPropertyPanel, openPropertyPanel, selectedId, components } = useStore();
 
   const currentComponent = useMemo(() => {
     return components.find((item) => item.id === selectedId);
   }, [components, selectedId]);
 
-  console.log(currentComponent, 'currentComponent');
+  // 获取当前组件分类，base基础组件都需要强制重新设置label和name，不然禁止关闭弹窗。
+  const getCurrentComponentType = useMemo(() => {
+    if (currentComponent) {
+      return getComponentCategory(currentComponent.name);
+    }
+  }, [currentComponent]);
+
+  console.log(currentComponent, 'currentComponent', getCurrentComponentType);
 
   const onClose = () => {
     setOpenPropertyPanel(false);
@@ -20,6 +29,6 @@ const ConfigPanel = () => {
       {selectedId}
     </Drawer>
   );
-};
+});
 
 export default ConfigPanel;
--- src/pages/Designer/index.tsx
+++ src/pages/Designer/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Layout } from 'antd';
 import { DndProvider } from 'react-dnd';
 import { ComponentProvider } from 'react-mario-core';
+import { useLocalObservable } from 'mobx-react-lite';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { FormDesignerStore, StoreContext } from '@/store/useStore';
 import ComponentPanel from './components/ComponentPanel';
 import Canvas from './components/Canvas';
 import ConfigPanel from './components/ConfigPanel';
@@ -10,20 +12,23 @@ import ConfigPanel from './components/ConfigPanel';
 const { Sider, Content } = Layout;
 
 const Designer: React.FC = () => {
+  const store = useLocalObservable(() => new FormDesignerStore());
   return (
-    <ComponentProvider components={[]}>
-      <DndProvider backend={HTML5Backend}>
-        <Layout style={{ height: '100vh' }}>
-          <Sider width={280} theme="light" style={{ borderRight: '1px solid #f0f0f0' }}>
-            <ComponentPanel />
-          </Sider>
-          <Content style={{ display: 'flex', flexDirection: 'column' }}>
-            <Canvas />
-          </Content>
-          <ConfigPanel />
-        </Layout>
-      </DndProvider>
-    </ComponentProvider>
+    <StoreContext.Provider value={store}>
+      <ComponentProvider components={[]}>
+        <DndProvider backend={HTML5Backend}>
+          <Layout style={{ height: '100vh' }}>
+            <Sider width={280} theme="light" style={{ borderRight: '1px solid #f0f0f0' }}>
+              <ComponentPanel />
+            </Sider>
+            <Content style={{ display: 'flex', flexDirection: 'column' }}>
+              <Canvas />
+            </Content>
+            <ConfigPanel />
+          </Layout>
+        </DndProvider>
+      </ComponentProvider>
+    </StoreContext.Provider>
   );
 };
 
